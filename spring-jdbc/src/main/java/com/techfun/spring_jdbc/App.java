4964295bd9526a1e9d045e90eeb53c4f37f483b7
maysithman <maysismhan@gmail.com>
2019-03-23 15:38:43 +0630

    first commit

---
+++ spring-jdbc/src/main/java/com/techfun/spring_jdbc/App.java
@@ -0,0 +1,13 @@
+package com.techfun.spring_jdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
