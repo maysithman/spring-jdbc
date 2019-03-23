42ee141abe5bbba632038f5fed5b0ef20ff30324
maysithman <maysismhan@gmail.com>
2019-03-23 16:18:11 +0630

    jdbc connection

---
+++ spring-jdbc/src/main/java/com/techfun/jdbc/connectionhelper/ConnectionHelper.java
@@ -0,0 +1,26 @@
+package com.techfun.jdbc.connectionhelper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionHelper {
+
+	private static final String DB_DRIVER = "org.postgresql.Driver";
+	private static final String DB_CONNECTION = "jdbc:postgresql://localhost:5432/JDBC_Test";
+	private static final String DB_USER = "postgres";
+	private static final String DB_PASSWORD = "admin";
+	
+	public static Connection getDBConnection() {
+		Connection dbConnection = null;
+		
+		try {
+			Class.forName(DB_DRIVER);	
+			dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+		}catch (ClassNotFoundException | SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		return dbConnection;
+	}
+}
