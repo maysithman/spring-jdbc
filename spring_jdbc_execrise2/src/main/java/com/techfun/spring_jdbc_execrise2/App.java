da4c2bc97ddac568bf8efc5ee8d9cf7f0b8ffb2a
maysithman <maysismhan@gmail.com>
2019-03-31 15:12:07 +0630

    initial project (spring_jdbc_execrise2)

---
+++ spring_jdbc_execrise2/src/main/java/com/techfun/spring_jdbc_execrise2/App.java
@@ -0,0 +1,13 @@
+package com.techfun.spring_jdbc_execrise2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
